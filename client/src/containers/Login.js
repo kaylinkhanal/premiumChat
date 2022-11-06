54111139c74b501104e5b23dd3aedc1102405423
mrroshan064 <roshanmahato351@gmil.com>
2022-11-06 15:52:14 +0545

    created login page

---
+++ client/src/containers/Login.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+import "./Login.css"
+
+const Login = () => {
+  const [userName, setUserName] = useState("")
+  const [password, setPassword] = useState("")
+
+  const loginHandler = () => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        userName: userName,
+        Password: password,
+
+      }),
+    };
+    fetch("http://localhost:8080/login", requestOptions);
+  };
+
+  return (
+    <div ClassName="formInline">
+      <form className="loginForm" action="">
+        <input type="text" placeholder="UserName" onKeyUp={(e) => setUserName(e.target.value)} />
+        <input type="password" placeholder="Password" onKeyUp={(e) => setPassword(e.target.value)} />
+        <button onClick={loginHandler}>Submit</button>
+      </form>
+
+    </div>
+  )
+}
+
+export default Login
