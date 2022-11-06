d09d7498971109da796f56eb97597ceebb9d3de9
kaylinkhanal10 <kaylin.k@foundationai.com>
2022-11-06 10:04:56 +0545

    added env

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
 // Serve frontend
