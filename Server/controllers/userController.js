a5c3ae2dd13e11735043cbc6fd7e94459b78c01c
Akash <akashranabhat05@gmail.com>
2022-11-06 10:42:41 +0545

    routes issue fixed

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -7,31 +7,13 @@ const User = require("../models/userModel");
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
-
-  if (!name || !email || !password) {
-    res.status(400);
-    throw new Error("Please add all fields");
-  }
-
-  // Check if user exists
-  const userExists = await User.findOne({ email });
-
-  if (userExists) {
-    res.status(400);
-    throw new Error("User already exists");
-  }
-
-  // Hash password
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
-
-  // Create user
   const user = await User.create({
     name,
     email,
     password: hashedPassword,
   });
-
   if (user) {
     res.status(201).json({
       _id: user.id,
--- Server/server.js
+++ Server/server.js
@@ -2,9 +2,8 @@ const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
-const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
-const port = process.env.PORT || 5005;
+const port = process.env.PORT || 3000;
 
 connectDB();
 
@@ -13,21 +12,6 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use("/api/users", require("./routes/userRoutes"));
-
-// Serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send("Please set to production"));
-}
-
-app.use(errorHandler);
+app.use("/register", require("./routes/userRoutes"));
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
