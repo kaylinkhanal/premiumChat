21889e12105feebf6ed2cd11c829e202c8ca1167
bibekjungthapa <bibekthapa007@hotmail.com>
2022-11-07 23:33:09 +0545

    FormikYupRegister Validation Done

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -1,12 +1,8 @@
-const bcrypt = require("bcryptjs");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
-
-// @desc    Register new user
-// @route   POST /api/users
-// @access  Public
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
+  const bcrypt = require("bcryptjs");
   const salt = await bcrypt.genSalt(10);
   const hashedPassword = await bcrypt.hash(password, salt);
   const user = await User.create({
@@ -26,36 +22,8 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
-// @desc    Authenticate a user
-// @route   POST /api/users/login
-// @access  Public
-// const loginUser = asyncHandler(async (req, res) => {
-//   const { email, password } = req.body;
 
-//   // Check for user email
-//   const user = await User.findOne({ email });
-
-//   if (user && (await bcrypt.compare(password, user.password))) {
-//     res.json({
-//       _id: user.id,
-//       name: user.name,
-//       email: user.email,
-//     });
-//   } else {
-//     res.status(400);
-//     throw new Error("Invalid credentials");
-//   }
-// });
-
-// @desc    Get user data
-// @route   GET /api/users/me
-// @access  Private
-// const getMe = asyncHandler(async (req, res) => {
-//   res.status(200).json(req.user);
-// });
 
 module.exports = {
-  registerUser,
-  //loginUser,
-  //getMe,
+  registerUser
 };
--- client/src/containers/Register.jsx
+++ client/src/containers/Register.jsx
@@ -1,60 +1,132 @@
-//import "./register.css";
-// import './Login'
-// import { Link} from "react-router-dom";
-import { useState } from "react";
+// import { useState } from "react";
+// function Register() {
+//   const [name, setname] = useState("");
+//   const [email, setemail] = useState("");
+//   const [password, setpassword] = useState("");
 
+//   const callingFunc = (e) => {
+//     const requestOptions = {
+//       method: "POST",
+//       headers: { "Content-Type": "application/json" },
+//       body: JSON.stringify({
+//         name: name,
+//         email: email,
+//         password: password,
+//       }),
+//     };
+//     fetch("http://localhost:3001/register", requestOptions);
+//   };
+
+//   return (
+//     <div className="login-box">
+//       <h2>Register</h2>
+//         <div className="user-box">
+//           <input
+//             type="text"
+//             name="name"
+//             onKeyUp={(e) => setname(e.target.value)}
+//           />
+//           <label>Username</label>
+//         </div>
+//         <div className="user-box">
+//           <input
+//             type="text"
+//             name="email"
+//             onKeyUp={(e) => setemail(e.target.value)}
+//           />
+//           <label>Email</label>
+//         </div>
+//         <div className="user-box">
+//           <input
+//             type="password"
+//             name="password"
+//             onKeyUp={(e) => setpassword(e.target.value)}
+//           />
+//           <label>Password</label>
+//         </div>
+       
+//         <button  onClick={()=>callingFunc()}>Submit</button>
+
+//     </div>
+//   );
+// }
+// export default Register;
+
+
+import React from 'react';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 function Register() {
-  const [name, setname] = useState("");
-  const [email, setemail] = useState("");
-  const [password, setpassword] = useState("");
 
-  const callingFunc = (e) => {
-    const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        name: name,
-        email: email,
-        password: password,
-      }),
-    };
-    fetch("http://localhost:3001/register", requestOptions);
-  };
+ 
+  
 
-  return (
-    <div className="login-box">
-      <h2>Register</h2>
-        <div className="user-box">
-          <input
-            type="text"
-            name="name"
-            onKeyUp={(e) => setname(e.target.value)}
-          />
-          <label>Username</label>
-        </div>
-        <div className="user-box">
-          <input
-            type="text"
-            name="email"
-            onKeyUp={(e) => setemail(e.target.value)}
-          />
-          <label>Email</label>
-        </div>
-        <div className="user-box">
-          <input
-            type="password"
-            name="password"
-            onKeyUp={(e) => setpassword(e.target.value)}
-          />
-          <label>Password</label>
-        </div>
-        {/* <div class="user-box">
-        <input type="password" name="" required=""/>
-        <label>Confirm Password</label>
-      </div> */}
-        <button  onClick={()=>callingFunc()}>Submit</button>
-        {/* <Link to="/">Already have an Account?</Link> */}
-    </div>
-  );
-}
+  const callingFunc = (values) => {
+        const requestOptions = {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            name:values.name ,
+            email:values.email,
+            password: values.password,
+          }),
+        };
+        fetch("http://localhost:3001/register", requestOptions);
+      };
+
+
+      const SignupSchema = Yup.object().shape({
+        name: Yup.string()
+          .min(1, 'Too Short!')
+          .max(20, 'Too Long!'),
+          // .required('Required'),
+        email: Yup.string().email('Invalid email').required('Required'),
+        password: Yup.string()
+        .min(2, 'Too Short!')
+        .max(50, 'Too Long!')
+        .required('Required'),
+      });
+return(
+  <div>
+    <h1>Signup</h1>
+    <Formik
+      initialValues={{
+       name : '',
+       email: '',
+        password: '',
+        
+      }}
+      validationSchema={SignupSchema}
+      onSubmit={values => {
+        // same shape as initial values
+        console.log(values);
+        callingFunc(values)
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form>
+ <label>Name:</label>
+          <Field name="name"  placeholder="Enter Your Name"/>
+         
+          {errors.name && touched.name ? (
+            <div>{errors.name}</div>
+          ) : null}
+       <label>Email:</label>
+          <Field name="email" type="email"  placeholder="Enter Your Email"/>
+         
+          {errors.email && touched.email ? <div>{errors.email}</div> : null}
+ <label>Password:</label>
+          <Field name="password"  placeholder="Enter Your Password"/>
+         
+          {errors.password && touched.password ? (
+            <div>{errors.password}</div>
+          ) : null}
+          <button type="submit"
+          onClick={()=>callingFunc()}
+          >Submit</button>
+        </Form>
+      )}
+    </Formik>
+  </div>
+)}
 export default Register;
