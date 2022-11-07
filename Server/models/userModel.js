126a102682a437a7559cf9b35dfa80c435416245
kiran pant <kiranpant9840@gmail.com>
2022-11-07 10:01:15 +0545

    multer imagage upload feature added

--- Server/models/userModel.js
+++ Server/models/userModel.js
@@ -15,6 +15,7 @@ const userSchema = mongoose.Schema(
       type: String,
       required: [true, 'Please add a password'],
     },
+    filename:{type:String, required:true},
   },
   {
     timestamps: true,
--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -6,7 +6,26 @@ const {
   getMe,
 } = require("../controllers/userController");
 
-router.post("/", registerUser);
+//multer file image upload
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../client/src/uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage }).single("avatar");
+
+
+
+
+
+
+
+
+router.post("/", upload,registerUser);
 //router.post("/login", loginUser);
 //router.get('/me', protect, getMe)
 
